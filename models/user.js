fd019b1cb18c0d4c84d07392e8ad1104aea3c6fc
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-02-01 18:41:22 +0100

    feat: add validalion login and registration schema

--- models/user.js
+++ models/user.js
@@ -34,8 +34,72 @@ userSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
+//validation Schema for Registration:
+const validRegisterSchema = Joi.object({
+  name: Joi.string().min(3).max(200).required().messages({
+    "string.base": `"name" should be a type of 'text'`,
+    "string.empty": `"name" cannot be an empty field`,
+    "string.min": `"name" should have a minimum length of {#limit}`,
+    "string.max": `"name" should have a maximum length of {#limit}`,
+    "any.required": `"name" is a required field`,
+  }),
+  email: Joi.string()
+    .min(5)
+    .max(254)
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "ua"] },
+    })
+    .required()
+    .messages({
+      "string.base": `"email" should be a type of 'text'`,
+      "string.empty": `"email" cannot be an empty field`,
+      "string.min": `"email" should have a minimum length of {#limit}`,
+      "string.max": `"email" should have a maximum length of {#limit}`,
+      "any.required": `"email" is a required field`,
+    }),
+  password: Joi.string()
+    .min(8)
+    .max(100)
+    .regex(/(?=.*[0-9])(?=.*[a-z])[0-9a-zA-Z]{8,}/)
+    .required()
+    .messages({
+      "string.base": `"password" should be a type of 'text'`,
+      "string.empty": `"password" cannot be an empty field`,
+      "string.min": `"password" should have a minimum length of {#limit}`,
+      "string.max": `"password" should have a maximum length of {#limit}`,
+      "any.required": `"password" is a required field`,
+    }),
+});
+
+//validation Schema for Login:
+const validLoginSchema = Joi.object({
+  email: Joi.string()
+    .min(5)
+    .max(254)
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "ua"] },
+    })
+    .required()
+    .messages({
+      "string.base": `"email" should be a type of 'text'`,
+      "string.empty": `"email" cannot be an empty field`,
+      "string.min": `"email" should have a minimum length of {#limit}`,
+      "string.max": `"email" should have a maximum length of {#limit}`,
+      "any.required": `"email" is a required field`,
+    }),
+  password: Joi.string().required().messages({
+    "string.base": `"password" should be a type of 'text'`,
+    "string.empty": `"password" cannot be an empty field`,
+    "any.required": `"password" is a required field`,
+  }),
+});
+
 const User = model("user", userSchema);
 
 module.exports = {
   User,
+  validRegisterSchema,
+  validLoginSchema,
 };
