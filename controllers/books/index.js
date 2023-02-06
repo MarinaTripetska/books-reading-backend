08f52bde53e8328e9f5ebb2029472b7e12b1586a
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-02-06 02:03:28 +0100

    feat: add get all books and create one book

--- controllers/books/index.js
+++ controllers/books/index.js
@@ -1,5 +1,7 @@
 const getAllProducts = require("./getAllBooks");
+const createBook = require("./createBook");
 
 module.exports = {
   getAllProducts,
+  createBook,
 };
--- services/index.js
+++ services/index.js
@@ -1,3 +1,4 @@
 const updateTokens = require("./updateTokens");
 const bookService = require("./bookService");
+
 module.exports = { updateTokens, bookService };
